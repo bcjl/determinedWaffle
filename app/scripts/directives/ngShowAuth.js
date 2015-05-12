73e630b877839f9c22523225d62fcd85172b3c2b
Brian <briancjliu@gmail.com>
2015-05-12 11:48:04 -0700

    Testing deployment

--- app/scripts/directives/ngShowAuth.js
+++ app/scripts/directives/ngShowAuth.js
@@ -14,7 +14,6 @@ angular.module('thumbsCheckApp')
       restrict: 'AE',
       link: function(scope, el) {
         el.addClass('ng-cloak'); // hide until we process it
-
         function update() {
           // sometimes if ngCloak exists on same element, they argue, so make sure that
           // this one always runs last for reliability
